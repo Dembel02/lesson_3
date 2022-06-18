08f5e687b35a55a96809d1b87527a4c43baee752
Dembel02 <IRGilyazov@yandex.ru>
2022-06-18 20:07:00 +0500

    dz_

--- victory.py
+++ victory.py
@@ -40,7 +40,8 @@ days = {'01': 'первое', '02': 'второе', '03': 'третье', '04': 
 months = {'01': 'января', '02': 'февраля', '03': 'марта', '04': 'апреля',
           '05': 'мая', '06': 'июня', '07': 'июля', '08': 'августа',
           '09': 'сентября', '10': 'октября', '11': 'ноября', '12': 'декабря'}
-result = random.choices(list(writers.keys()), k=5)
+result = random.sample(writers.keys(), 5)
+#result = random.choices(list(writers.keys()), k=5)
 answer = ()
 count = 0
 for man in result:
